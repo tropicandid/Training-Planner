a4fd9bd7a3935a94f5fa02c5ec262c799137d6ac
Deanna Steers <dsteers@ad.dealeron.com>
2017-09-01 11:37:03 -0400

    TP-6: Adding initial solution content and structure. Still a lot to be done to flesh out the components and mvc frame

---
+++ TrainingPlanner/TrainingPlanner/Components/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPlanner.Components
+{
+  public class Image
+  {
+    public string ImageUrl { get; set; }
+  }
+}
---
+++ TrainingPlanner/TrainingPlanner/Components/Tags.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingPlanner.Components
+{
+  public class Tags
+  {
+    public ExerciseTagTypes ExerciseTags { get; set; }
+  }
+  public enum ExerciseTagTypes
+  {
+    // Different types of workouts EX: plyometric, cardio, yoga, etc.
+  }
+}
